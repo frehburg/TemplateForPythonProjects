5f3d22af7fa37c2ce379a1a5ad85b1327b232146
frehburg <fil.rehburg@gmail.com>
2024-07-22 13:03:02 +0200

    updated pydoc of plus and minus

--- src/template_for_python_projects/example/minus.py
+++ src/template_for_python_projects/example/minus.py
@@ -1,14 +1,12 @@
-import template_for_python_projects
-
-
 def minus(a: int, b: int) -> int:
     """
     This function returns the difference of two numbers.
 
-    Very similar to :func:`template_for_python_projects.example.plus.plus` but returns the difference instead of the sum.
+    Very similar to :func:`template_for_python_projects.example.plus.plus` but returns the difference instead of the
+    sum.
 
-    :param a: a number
-    :param b: another number
-    :return: a third number
+    :param a: an integer number
+    :param b: another integer number
+    :return: the difference of the two numbers
     """
     return a - b
--- src/template_for_python_projects/example/plus.py
+++ src/template_for_python_projects/example/plus.py
@@ -1,11 +1,12 @@
 def plus(a: int, b: int) -> int:
     """
-    This function returns the difference of two numbers.
-    :param a: a number
-    :type a: int
-    :param b: another number
-    :type b: int
-    :return: a third number
-    :rtype: int
+    This function returns the sum of two numbers.
+
+    Very similar to :func:`template_for_python_projects.example.minus.minus` but returns the sum instead of the
+    difference.
+
+    :param a: an integer number
+    :param b: another integer number
+    :return: the sum of the two numbers `a` and `b`
     """
     return a + b
